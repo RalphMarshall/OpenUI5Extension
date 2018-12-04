93d12f6c1e261756a17c3098ddcb4c0770511ed6
Ralph Marshall <Ralph.Marshall@sas.com>
2018-12-04 10:23:18 -0500

    First working version

--- popup.js
+++ popup.js
@@ -1,17 +1,17 @@
 // popup.js
 
-sap.ui.define([
-	'sap/ui/core/Core'
-], function(
-    Core
-) {
+sap.ui.define('popup', [
+    'sap/ui/core/Core'
+],
+    function(Core) {
 
-    sap.ui.getCore().attachInit(function() {
-        new sap.m.Button({
-            text: "Say 'Hello'",
-            press: function() {
-                sap.m.MessageToast.show("Hello, world!");
-            }
-        }).placeAt("content");
-    });
-});
+        sap.ui.getCore().attachInit(function() {
+            new sap.m.Button({
+                text: "Say 'Hello'",
+                press: function() {
+                    sap.m.MessageToast.show("Hello, world!");
+                }
+            }).placeAt("content");
+        });
+    }
+);
